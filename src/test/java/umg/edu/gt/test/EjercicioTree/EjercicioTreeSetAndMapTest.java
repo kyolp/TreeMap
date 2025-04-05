493be82d5372d6eab62d26a4e507c5c65074a3fb
David Yol <kyolp@miumg.edu.gt>
2025-04-05 00:39:36 -0600

    COMMIT DE EJERCICIO TREESETANDMAP2

--- src/test/java/umg/edu/gt/test/EjercicioTree/EjercicioTreeSetAndMapTest.java
+++ src/test/java/umg/edu/gt/test/EjercicioTree/EjercicioTreeSetAndMapTest.java
@@ -15,45 +15,75 @@ public class EjercicioTreeSetAndMapTest {
 
 @Test
 public void testFrequenciaPalabras() throws IOException {
-    String rutaArchivo = "palabras.txt";
-
+    String rutaArchivo = "C:/Users/David/Documents/NetBeansProjects/EstructuraDeDatos2025/palabras.txt";
     Map<String, Integer> resultado = EjercicioTreeSetAndMap.contarFrecuencia(rutaArchivo);
 
-    assertEquals(2, resultado.get("hola"));   // OK
-    assertEquals(2, resultado.get("mundo"));  // OK
-    assertEquals(2, resultado.get("java"));   // OK
+    System.out.println("=== Frecuencia de Palabras ===");
+    for (Map.Entry<String, Integer> entry : resultado.entrySet()) {
+        System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+
+    assertEquals(2, resultado.get("hola"));
+    assertEquals(2, resultado.get("mundo"));
+    assertEquals(2, resultado.get("java"));
 }
 
-    @Test
-    public void testRastreadorVersiones() {
-        EjercicioTreeSetAndMap.agregarVersion("System.out.println(\"Hola Mundo\");");
-        EjercicioTreeSetAndMap.agregarVersion("System.out.println(\"Hola Java\");");
+@Test
+public void testRastreadorVersiones() {
+    int v1 = EjercicioTreeSetAndMap.agregarVersion("System.out.println('Hola Mundo');");
+    int v2 = EjercicioTreeSetAndMap.agregarVersion("System.out.println('Hola Java');");
 
-        assertEquals("System.out.println(\"Hola Mundo\");", EjercicioTreeSetAndMap.obtenerVersion(1));
-        assertEquals("System.out.println(\"Hola Java\");", EjercicioTreeSetAndMap.obtenerUltimaVersion());
+    String ultima = EjercicioTreeSetAndMap.obtenerUltimaVersion();
+    String primera = EjercicioTreeSetAndMap.obtenerVersion(1);
+
+    System.out.println("Agregar version " + v1 + ": " + EjercicioTreeSetAndMap.obtenerVersion(v1));
+    System.out.println("Agregar version " + v2 + ": " + EjercicioTreeSetAndMap.obtenerVersion(v2));
+    System.out.println("Ultima version: " + ultima);
+    System.out.println("Version 1: " + primera);
+
+    assertEquals("System.out.println('Hola Mundo');", primera);
+    assertEquals("System.out.println('Hola Java');", ultima);
+}
+ 
+
+@Test
+public void testEventosOrdenCronologico() {
+    EjercicioTreeSetAndMap.agregarEvento(
+        new EjercicioTreeSetAndMap.Evento(LocalDateTime.of(2025, 5, 10, 10, 0),
+            "Conferencia de Java", "Auditorio A")
+    );
+    EjercicioTreeSetAndMap.agregarEvento(
+        new EjercicioTreeSetAndMap.Evento(LocalDateTime.of(2025, 4, 15, 14, 0),
+            "Taller de Machine Learning", "Sala 3")
+    );
+
+    var eventos = EjercicioTreeSetAndMap.obtenerEventos();
+    EjercicioTreeSetAndMap.Evento proximo = EjercicioTreeSetAndMap.obtenerProximoEvento();
+
+    System.out.println("Proximo evento: " + proximo);
+
+    System.out.println("Lista de eventos:");
+    int i = 1;
+    for (EjercicioTreeSetAndMap.Evento e : eventos) {
+        System.out.println(i++ + ". " + e);
     }
 
-    @Test
-    public void testEventosOrdenCronologico() {
-        EjercicioTreeSetAndMap.agregarEvento(
-                new EjercicioTreeSetAndMap.Evento(LocalDateTime.of(2025, 5, 10, 10, 0),
-                        "Conferencia de Java", "Auditorio A")
-        );
-        EjercicioTreeSetAndMap.agregarEvento(
-                new EjercicioTreeSetAndMap.Evento(LocalDateTime.of(2025, 4, 15, 14, 0),
-                        "Taller de Machine Learning", "Sala 3")
-        );
+    assertEquals("Taller de Machine Learning", proximo.getNombre());
+}
 
-        var eventos = EjercicioTreeSetAndMap.obtenerEventos();
-        assertEquals(2, eventos.size());
-
-        EjercicioTreeSetAndMap.Evento proximo = EjercicioTreeSetAndMap.obtenerProximoEvento();
-        assertEquals("Taller de Machine Learning", proximo.getNombre()); // OK
-    }
 
     // 🚨 Test forzado a fallar
     @Test
     public void testFallaFuerzaBruta() {
         assertEquals(999, EjercicioTreeSetAndMap.agregarVersion("Test falso")); // Este fallará intencionalmente
     }
+    
+    
 }
+
+
+
+
+
+            
+            
